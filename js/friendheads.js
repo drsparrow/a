39cafa9b6664b506a1c907c928527ff491eb79b8
Ari Brenner <aribrenner@gmail.com>
2018-10-20 16:59:45 -0400

    wip

--- js/friendheads.js
+++ js/friendheads.js
@@ -1,12 +1,18 @@
 $(function () {
+    setTimeout(function () {
+        var ariHeads = new Friendheads('ari');
 
-    var ariHeads = new Friendheads('ari');
+        ariHeads.opacity(0);
+        ariHeads.add(2);
 
-    ariHeads.add(2);
+        setTimeout(function () {
+            ariHeads.opacity(1);
+        }, 2000);
 
-    $('.more-ari, .ari-face').click(function () {
-        ariHeads.add();
-        return false;
-    });
+        $('.more-ari, .ari-face').click(function () {
+            ariHeads.add();
+            return false;
+        });
 
+    }, 1000);
 });
